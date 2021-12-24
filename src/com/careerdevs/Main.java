20c3c438b315625c6094082e8a17b0ebf6d4ce4f
olima284 <olivialima.student@careerdevs.com>
2021-12-23 19:01:58 -0500

    completed get first Algorithm

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -3,6 +3,12 @@ package com.careerdevs;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int test1 = getFirstElement(new int[] {1,2,3});
+        System.out.println(test1);
     }
+    public static int getFirstElement(int[] arr){
+        int firstNum = arr[0];
+        return firstNum;
+    }
+
 }
