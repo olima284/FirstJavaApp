dc0d0603bb68b9f78915b084c0f2ccdc249e2c27
olima284 <olivialima.student@careerdevs.com>
2022-01-03 12:49:00 -0500

    completed get second Algorithm

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -3,12 +3,25 @@ package com.careerdevs;
 public class Main {
 
     public static void main(String[] args) {
-	    int test1 = getFirstElement(new int[] {1,2,3});
-        System.out.println(test1);
+	    //int test1 = getFirstElement(new int[] {1,2,3});
+        //System.out.println(test1);
     }
     public static int getFirstElement(int[] arr){
         int firstNum = arr[0];
         return firstNum;
     }
+    public static int addition(int num) {
+        return ++num;
 
+    }
+    public static int search(int arr[], int item) {
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == item) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
+
